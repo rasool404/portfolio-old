852f9f5cef9adfbb446d72a455d1670570acf8cb
Rasul <rasuljonhalimov47@gmail.com>
2021-12-08 18:36:59 +0500

    add russian lang

---
+++ js/lang.js
@@ -0,0 +1,191 @@
+const langArr = {
+  "header-home": {
+    en: "Home",
+    ru: "Домой",
+  },
+  "header-skills": {
+    en: "Skills & Tools",
+    ru: "Навыки & Инструменты",
+  },
+  "header-about": {
+    en: "About me",
+    ru: "Обо мне",
+  },
+  "header-works": {
+    en: "Works",
+    ru: "Работы",
+  },
+  "header-contact": {
+    en: "Contact",
+    ru: "Контакты",
+  },
+  "header-greeting-title": {
+    en: "HI THERE 👋",
+    ru: "Привет 👋",
+  },
+  "header-greeting-name": {
+    en: `I'm <span id="name"></span>`,
+    ru: `Я <span id="name"></span>`,
+  },
+  "header-profession": {
+    en: "Front-End Developer",
+    ru: "Front-End Разработчик",
+  },
+  "header-descr": {
+    en: `I've been doing front-end development for a year now. Do you need a
+    site layout, or maybe a turnkey website? Then contact me`,
+    ru: `Уже год занимаюсь фронтенд-разработкой. Вам нужен
+    верстка сайта, а может сайт под ключ? Тогда свяжитесь со мной`,
+  },
+  "header-btn": {
+    en: "Contact Me",
+    ru: "Написать мне",
+  },
+  "skills-title": {
+    en: "SKILLS & TOOLS",
+    ru: "Навыки & Инструменты",
+  },
+  "skills-descr": {
+    en: `The skills, tools and technologies I use to bring your products to
+    life:`,
+    ru: `Навыки, инструменты и технологии, которые я использую, чтобы представить вашу продукцию
+    жизнь:`,
+  },
+  "currently-title": {
+    en: "Currently working on:",
+    ru: "В настоящее время работаю над:",
+  },
+  "currently-descr": {
+    en: `Improving my skills in, and understanding of vanilla JavaScript
+    and React.`,
+    ru: `Улучшение моих навыков и понимания ванильного JavaScript
+    и React.`,
+  },
+  "about-title": {
+    en: "About me",
+    ru: "Обо мне",
+  },
+  "about-text": {
+    en: `<span>A curiosity-driven Front-End Developer</span>
+    I'm passionate about bringing both the technical and visual
+    aspects of digital products to life. User experience, beautiful
+    pixels and writing clean accessible, human code matters to me. I
+    sweat the details. <br />
+    <br />
+    I'm happiest when I'm creating, learning, exploring and thinking
+    about how to make things better. <br />
+    <br />
+    Why programming? Everything is elementary - I love it, the
+    profession of the future, thanks to which I can provide myself
+    and fulfill my dream - travel, at the moment I specialize in
+    front-end and back-end development. Why should you choose me? I
+    approach each order with great responsibility and love, as I
+    want to make a name for myself, exclude plagiarism and
+    negligence, fully study the project, the client and its target
+    audience, work for quality, trying to make an order as quickly
+    and uniquely as possible, taking into account all the edits and
+    wishes. By trusting me, you will get the maximum return for your
+    project, save your nerves and time.`,
+    ru: `<span> Фронтенд-разработчик, движимый любопытством </span>
+    Я с энтузиазмом отношусь к техническим и визуальным
+    аспектам цифровых продуктов. User Experience, красивые
+    пиксели и написание чистого, доступного, человеческого кода имеют значение для меня. я
+    попотеть в деталях. <br />
+    <br />
+    Я счастлив, когда создаю, учусь, исследую и думаю
+    о том, как улучшить положение. <br />
+    <br />
+    Почему программирование? Все элементарно - мне это нравится,
+    профессия будущего, благодаря которой я могу обеспечить себя
+    и осуществить свою мечту - путешествовать, на данный момент специализируюсь на
+    front-end и back-end разработкe. Почему вы должены выбрать меня? Я
+    подхожу к каждому заказу с большой ответственностью и любовью, так как я
+    хочу сделать себе имя, исключить плагиат и
+    халатность. Я полностью изучаю проект, клиента и его цель. Работаю на качество, стараюсь сделать заказ максимально быстро
+    и по возможности однозначно с учетом всех правок и
+    пожеланий. Доверившись мне, вы получите максимальную отдачу от
+    проектф, сэкономите нервы и время.`,
+  },
+  "works-title": {
+    en: "Works",
+    ru: "Работы",
+  },
+  "works-descr": {
+    en: "A Selection Of Stuff I've Built",
+    ru: "Подборка моих работ",
+  },
+  "work-1-title": {
+    en: "Gitpod",
+    ru: "Gitpod",
+  },
+  "work-1-descr": {
+    en: `Gitpod streamlines developer workflows by providing prebuilt,
+    collaborative developer environments in your browser - powered
+    by VS Code.`,
+    ru: `Gitpod упрощает рабочие процессы разработчика, предоставляя готовые,
+    среды совместной разработки в вашем браузере - на базе
+    пользователя VS Code.`,
+  },
+  "work-2-title": {
+    en: "Online Store React",
+    ru: "Online Store React",
+  },
+  "work-2-descr": {
+    en: `The best online store in the world. <br />
+    Online store written in react and node js. <br />
+    Multifunctional and easy to use`,
+    ru: `Лучший интернет-магазин в мире. <br />
+    Интернет-магазин написан на react и node js. <br />
+    Многофункциональный и простой в использовании.`,
+  },
+  "work-1-btn-text1": {
+    en: "Visit",
+    ru: "Открыть",
+  },
+  "work-1-btn-text2": {
+    en: "View It",
+    ru: "Посмотреть",
+  },
+  "work-2-btn-text1": {
+    en: "Visit",
+    ru: "Открыть",
+  },
+  "work-2-btn-text2": {
+    en: "View It",
+    ru: "Посмотреть",
+  },
+  "contact-title": {
+    en: "Contact",
+    ru: "Контакты",
+  },
+  "contact-descr": {
+    en: "Get In Touch",
+    ru: "Написать",
+  },
+  "contact-text": {
+    en: `I am available to work on your projects and bring your ideas to
+    life. I am just a click away.`,
+    ru: `Я готов работать над вашими проектами и воплощать ваши идеи
+    в жизнь. Я на расстоянии одного клика.`,
+  },
+  "contact-form-text": {
+    en: "Fill in your info in the form below and I look forward to hearing from you!",
+    ru: "Заполните свою информацию в форме ниже, и я с нетерпением жду вашего ответа!",
+  },
+  "contact-form-btn": {
+    en: "send email",
+    ru: "отправить email",
+  },
+  "contact-social": {
+    en: "Feeling social? Find me on these online spaces too!",
+    ru: "Чувствуете себя социальным? Найдите меня и на этих онлайн-площадках!",
+  },
+  "footer-copyright": {
+    en: `&copy; Copyright 2021. All right reserved`,
+    ru: `&copy; Copyright 2021. Все права защищены.`,
+  },
+  "footer-contact": {
+    en: "Contact:",
+    ru: "Контакты:",
+  },
+};
--- js/main.js
+++ js/main.js
@@ -12,11 +12,7 @@ $(document).ready(function () {
 
   $(".loader").addClass("hide");
 
-  window.addEventListener("scroll", function () {
-    const navScroll = document.getElementById("header");
-
-    navScroll.classList.toggle("fixed", window.scrollY > 5);
-  });
+  makeHeaderFixed();
 
   $(".portfolio__slider").slick({
     dots: true,
@@ -49,15 +45,6 @@ $(document).ready(function () {
     }
     return false;
   });
-
-  let i = 0;
-  const txt = "   Rasul";
-
-  setInterval(() => {
-    const el = document.getElementById("name");
-    el.append(txt.charAt(i));
-    i++;
-  }, 100);
 
   particlesJS("particles", {
     particles: {
@@ -178,203 +165,261 @@ $(document).ready(function () {
 
   AOS.init({});
 
-});
-
-//FORMS
-function forms() {
-  //FIELDS
-  $("input,textarea").focus(function () {
-    if ($(this).val() == $(this).attr("data-value")) {
-      $(this).addClass("focus");
-      $(this).parent().addClass("focus");
-      if ($(this).attr("data-type") == "pass") {
-        $(this).attr("type", "password");
-      }
-      $(this).val("");
-    }
-    removeError($(this));
-  });
-  $("input[data-value], textarea[data-value]").each(function () {
-    if (this.value == "" || this.value == $(this).attr("data-value")) {
-      if (
-        $(this).hasClass("l") &&
-        $(this).parent().find(".form__label").length == 0
-      ) {
-        $(this)
-          .parent()
-          .append(
-            '<div class="form__label">' + $(this).attr("data-value") + "</div>"
-          );
-      } else {
-        this.value = $(this).attr("data-value");
-      }
-    }
-    if (this.value != $(this).attr("data-value") && this.value != "") {
-      $(this).addClass("focus");
-      $(this).parent().addClass("focus");
-      if (
-        $(this).hasClass("l") &&
-        $(this).parent().find(".form__label").length == 0
-      ) {
-        $(this)
-          .parent()
-          .append(
-            '<div class="form__label">' + $(this).attr("data-value") + "</div>"
-          );
-      }
-    }
-
-    $(this).click(function () {
-      if (this.value == $(this).attr("data-value")) {
+  //FORMS
+  function forms() {
+    //FIELDS
+    $("input,textarea").focus(function () {
+      if ($(this).val() == $(this).attr("data-value")) {
+        $(this).addClass("focus");
+        $(this).parent().addClass("focus");
         if ($(this).attr("data-type") == "pass") {
           $(this).attr("type", "password");
         }
-        this.value = "";
+        $(this).val("");
       }
+      removeError($(this));
     });
-    $(this).blur(function () {
-      if (this.value == "") {
-        if (!$(this).hasClass("l")) {
+    $("input[data-value], textarea[data-value]").each(function () {
+      if (this.value == "" || this.value == $(this).attr("data-value")) {
+        if (
+          $(this).hasClass("l") &&
+          $(this).parent().find(".form__label").length == 0
+        ) {
+          $(this)
+            .parent()
+            .append(
+              '<div class="form__label">' +
+                $(this).attr("data-value") +
+                "</div>"
+            );
+        } else {
           this.value = $(this).attr("data-value");
         }
-        $(this).removeClass("focus");
-        $(this).parent().removeClass("focus");
-        if ($(this).attr("data-type") == "pass") {
-          $(this).attr("type", "text");
+      }
+      if (this.value != $(this).attr("data-value") && this.value != "") {
+        $(this).addClass("focus");
+        $(this).parent().addClass("focus");
+        if (
+          $(this).hasClass("l") &&
+          $(this).parent().find(".form__label").length == 0
+        ) {
+          $(this)
+            .parent()
+            .append(
+              '<div class="form__label">' +
+                $(this).attr("data-value") +
+                "</div>"
+            );
         }
       }
-      if ($(this).hasClass("vn")) {
-        formValidate($(this));
-      }
+
+      $(this).click(function () {
+        if (this.value == $(this).attr("data-value")) {
+          if ($(this).attr("data-type") == "pass") {
+            $(this).attr("type", "password");
+          }
+          this.value = "";
+        }
+      });
+      $(this).blur(function () {
+        if (this.value == "") {
+          if (!$(this).hasClass("l")) {
+            this.value = $(this).attr("data-value");
+          }
+          $(this).removeClass("focus");
+          $(this).parent().removeClass("focus");
+          if ($(this).attr("data-type") == "pass") {
+            $(this).attr("type", "text");
+          }
+        }
+        if ($(this).hasClass("vn")) {
+          formValidate($(this));
+        }
+      });
     });
-  });
-}
-forms();
+  }
+  forms();
 
-//VALIDATE FORMS
-$("form button[type=submit]").click(function () {
-  var er = 0;
-  var form = $(this).parents("form");
-  var ms = form.data("ms");
-  $.each(form.find(".req"), function (index, val) {
-    er += formValidate($(this));
-  });
-  if (er == 0) {
-    removeFormError(form);
+  //VALIDATE FORMS
+  $("form button[type=submit]").click(function () {
+    var er = 0;
+    var form = $(this).parents("form");
+    var ms = form.data("ms");
+    $.each(form.find(".req"), function (index, val) {
+      er += formValidate($(this));
+    });
+    if (er == 0) {
+      removeFormError(form);
 
-    if (ms != null && ms != "") {
-      showMessageByClass(ms);
+      if (ms != null && ms != "") {
+        showMessageByClass(ms);
+        return false;
+      }
+    } else {
       return false;
     }
-  } else {
-    return false;
+  });
+  function formValidate(input) {
+    var er = 0;
+    var form = input.parents("form");
+    if (input.attr("name") == "email" || input.hasClass("email")) {
+      if (input.val() != input.attr("data-value")) {
+        var em = input.val().replace(" ", "");
+        input.val(em);
+      }
+      if (
+        !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.val()) ||
+        input.val() == input.attr("data-value")
+      ) {
+        er++;
+        addError(input);
+      } else {
+        removeError(input);
+      }
+    } else {
+      if (input.val() == "" || input.val() == input.attr("data-value")) {
+        er++;
+        addError(input);
+      } else {
+        removeError(input);
+      }
+    }
+    if (input.attr("type") == "checkbox") {
+      if (input.prop("checked") == true) {
+        input.removeClass("err").parent().removeClass("err");
+      } else {
+        er++;
+        input.addClass("err").parent().addClass("err");
+      }
+    }
+    if (input.hasClass("name")) {
+      if (!/^[А-Яа-яa-zA-Z-]+( [А-Яа-яa-zA-Z-]+)$/.test(input.val())) {
+        er++;
+        addError(input);
+      }
+    }
+    if (input.hasClass("pass-2")) {
+      if (form.find(".pass-1").val() != form.find(".pass-2").val()) {
+        addError(input);
+      } else {
+        removeError(input);
+      }
+    }
+    return er;
+  }
+
+  function addError(input) {
+    input.addClass("err");
+    input.parent().addClass("err");
+    input.parent().find(".form__error").remove();
+    if (input.hasClass("email")) {
+      var error = "";
+      if (input.val() == "" || input.val() == input.attr("data-value")) {
+        error = input.data("error");
+      } else {
+        error = input.data("error");
+      }
+      if (error != null) {
+        input.parent().append('<div class="form__error">' + error + "</div>");
+      }
+    } else {
+      if (
+        input.data("error") != null &&
+        input.parent().find(".form__error").length == 0
+      ) {
+        input
+          .parent()
+          .append('<div class="form__error">' + input.data("error") + "</div>");
+      }
+    }
+    if (input.parents(".select-block").length > 0) {
+      input.parents(".select-block").parent().addClass("err");
+      input.parents(".select-block").find(".select").addClass("err");
+    }
+  }
+  function addErrorByName(form, input__name, error_text) {
+    var input = form.find('[name="' + input__name + '"]');
+    input.attr("data-error", error_text);
+    addError(input);
+  }
+  function addFormError(form, error_text) {
+    form.find(".form__generalerror").show().html(error_text);
+  }
+  function removeFormError(form) {
+    form.find(".form__generalerror").hide().html("");
+  }
+  function removeError(input) {
+    input.removeClass("err");
+    input.parent().removeClass("err");
+    input.parent().find(".form__error").remove();
+
+    if (input.parents(".select-block").length > 0) {
+      input.parents(".select-block").parent().removeClass("err");
+      input
+        .parents(".select-block")
+        .find(".select")
+        .removeClass("err")
+        .removeClass("active");
+      //input.parents('.select-block').find('.select-options').hide();
+    }
+  }
+  function removeFormErrors(form) {
+    form.find(".err").removeClass("err");
+    form.find(".form__error").remove();
   }
 });
-function formValidate(input) {
-  var er = 0;
-  var form = input.parents("form");
-  if (input.attr("name") == "email" || input.hasClass("email")) {
-    if (input.val() != input.attr("data-value")) {
-      var em = input.val().replace(" ", "");
-      input.val(em);
-    }
-    if (
-      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.val()) ||
-      input.val() == input.attr("data-value")
-    ) {
-      er++;
-      addError(input);
-    } else {
-      removeError(input);
-    }
-  } else {
-    if (input.val() == "" || input.val() == input.attr("data-value")) {
-      er++;
-      addError(input);
-    } else {
-      removeError(input);
-    }
-  }
-  if (input.attr("type") == "checkbox") {
-    if (input.prop("checked") == true) {
-      input.removeClass("err").parent().removeClass("err");
-    } else {
-      er++;
-      input.addClass("err").parent().addClass("err");
-    }
-  }
-  if (input.hasClass("name")) {
-    if (!/^[А-Яа-яa-zA-Z-]+( [А-Яа-яa-zA-Z-]+)$/.test(input.val())) {
-      er++;
-      addError(input);
-    }
-  }
-  if (input.hasClass("pass-2")) {
-    if (form.find(".pass-1").val() != form.find(".pass-2").val()) {
-      addError(input);
-    } else {
-      removeError(input);
-    }
-  }
-  return er;
+
+// Multilang
+const select = document.querySelector("select");
+const allLang = ["en", "ru"];
+
+select.addEventListener("change", changeURLLanguage);
+
+// перенаправить на url с указанием языка
+function changeURLLanguage() {
+  let lang = select.value;
+  location.href = window.location.pathname + "#" + lang;
+  location.reload();
 }
 
-function addError(input) {
-  input.addClass("err");
-  input.parent().addClass("err");
-  input.parent().find(".form__error").remove();
-  if (input.hasClass("email")) {
-    var error = "";
-    if (input.val() == "" || input.val() == input.attr("data-value")) {
-      error = input.data("error");
-    } else {
-      error = input.data("error");
-    }
-    if (error != null) {
-      input.parent().append('<div class="form__error">' + error + "</div>");
-    }
-  } else {
-    if (
-      input.data("error") != null &&
-      input.parent().find(".form__error").length == 0
-    ) {
-      input
-        .parent()
-        .append('<div class="form__error">' + input.data("error") + "</div>");
-    }
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substr(1);
+  console.log(hash);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + "#en";
+    location.reload();
   }
-  if (input.parents(".select-block").length > 0) {
-    input.parents(".select-block").parent().addClass("err");
-    input.parents(".select-block").find(".select").addClass("err");
-  }
-}
-function addErrorByName(form, input__name, error_text) {
-  var input = form.find('[name="' + input__name + '"]');
-  input.attr("data-error", error_text);
-  addError(input);
-}
-function addFormError(form, error_text) {
-  form.find(".form__generalerror").show().html(error_text);
-}
-function removeFormError(form) {
-  form.find(".form__generalerror").hide().html("");
-}
-function removeError(input) {
-  input.removeClass("err");
-  input.parent().removeClass("err");
-  input.parent().find(".form__error").remove();
 
-  if (input.parents(".select-block").length > 0) {
-    input.parents(".select-block").parent().removeClass("err");
-    input
-      .parents(".select-block")
-      .find(".select")
-      .removeClass("err")
-      .removeClass("active");
-    //input.parents('.select-block').find('.select-options').hide();
+  select.value = hash;
+  for (let key in langArr) {
+    let elem = document.querySelector(".lng-" + key);
+    if (elem) {
+      elem.innerHTML = langArr[key][hash];
+    }
   }
+
+  let i = 0;
+  let txt;
+
+  if (hash == "en") {
+    txt = "     Rasul";
+  } else {
+    txt = "     Расул";
+  }
+  setInterval(() => {
+    const el = document.getElementById("name");
+    el.append(txt.charAt(i));
+    i++;
+  }, 100);
 }
-function removeFormErrors(form) {
-  form.find(".err").removeClass("err");
-  form.find(".form__error").remove();
+
+changeLanguage();
+
+function makeHeaderFixed() {
+  window.addEventListener("scroll", function () {
+    const navScroll = document.getElementById("header");
+
+    navScroll.classList.toggle("fixed", window.scrollY > 5);
+  });
 }
